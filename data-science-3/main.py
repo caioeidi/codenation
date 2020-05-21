82b91b375b6cd07ca372d71cf011a0c4f6487fa4
Caio Eidi Tsunechiro <caioeidi@gmail.com>
2020-05-20 21:53:35 -0300

    Add files via upload

---
+++ data-science-3/main.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Desafio 5
+# 
+# Neste desafio, vamos praticar sobre redução de dimensionalidade com PCA e seleção de variáveis com RFE. Utilizaremos o _data set_ [Fifa 2019](https://www.kaggle.com/karangadiya/fifa19), contendo originalmente 89 variáveis de mais de 18 mil jogadores do _game_ FIFA 2019.
+# 
+# > Obs.: Por favor, não modifique o nome das funções de resposta.
+
+# ## _Setup_ geral
+
+# In[1]:
+
+
+from math import sqrt
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as sct
+import seaborn as sns
+#import statsmodels.api as sm
+#import statsmodels.stats as st
+from sklearn.decomposition import PCA
+
+#from loguru import logger
+
+
+# In[2]:
+
+
+# Algumas configurações para o matplotlib.
+#%matplotlib inline
+
+from IPython.core.pylabtools import figsize
+
+
+figsize(12, 8)
+
+sns.set()
+
+
+# In[3]:
+
+
+fifa = pd.read_csv("fifa.csv")
+
+
+# In[4]:
+
+
+columns_to_drop = ["Unnamed: 0", "ID", "Name", "Photo", "Nationality", "Flag",
+                   "Club", "Club Logo", "Value", "Wage", "Special", "Preferred Foot",
+                   "International Reputation", "Weak Foot", "Skill Moves", "Work Rate",
+                   "Body Type", "Real Face", "Position", "Jersey Number", "Joined",
+                   "Loaned From", "Contract Valid Until", "Height", "Weight", "LS",
+                   "ST", "RS", "LW", "LF", "CF", "RF", "RW", "LAM", "CAM", "RAM", "LM",
+                   "LCM", "CM", "RCM", "RM", "LWB", "LDM", "CDM", "RDM", "RWB", "LB", "LCB",
+                   "CB", "RCB", "RB", "Release Clause"
+]
+
+try:
+    fifa.drop(columns_to_drop, axis=1, inplace=True)
+except KeyError:
+    logger.warning(f"Columns already dropped")
+
+
+# ## Inicia sua análise a partir daqui
+
+# In[5]:
+
+
+fifa.head(100)
+
+
+# In[6]:
+
+
+aux = pd.DataFrame({'colunas' : fifa.columns,
+                   'tipo' : fifa.dtypes,
+                   'missing' : fifa.isna().sum(),
+                   'size' : fifa.shape[0],
+                   'unicos' : fifa.nunique()})
+aux
+
+
+# In[7]:
+
+
+fifa.dropna(inplace = True)
+fifa.corr().round(2)
+
+
+# In[8]:
+
+
+sns.heatmap(fifa.corr().round(2),  annot=True, annot_kws={"size": 7})
+
+
+# In[9]:
+
+
+fifa_standardize = ((fifa - fifa.mean())/fifa.std())
+fifa_standardize
+
+
+# In[10]:
+
+
+sns.heatmap(fifa_standardize.corr().round(2),  annot=True, annot_kws={"size": 7})
+
+
+# ## Questão 1
+# 
+# Qual fração da variância consegue ser explicada pelo primeiro componente principal de `fifa`? Responda como um único float (entre 0 e 1) arredondado para três casas decimais.
+
+# In[11]:
+
+
+fifa.dropna(inplace = True)
+fifa_standardize = ((fifa - fifa.mean())/fifa.std())
+pca = PCA(n_components = 1)
+first_principal_component = pca.fit_transform(fifa_standardize)
+evr = pca.explained_variance_ratio_
+float(round(evr[0],3))
+
+
+# In[23]:
+
+
+def q1():
+    fifa.dropna(inplace = True)
+    #fifa_standardize = ((fifa - fifa.mean())/fifa.std())
+    pca = PCA(n_components = 1)
+    first_principal_component = pca.fit(fifa)
+    evr = pca.explained_variance_ratio_
+    return float(round(evr[0],3))
+
+
+# ## Questão 2
+# 
+# Quantos componentes principais precisamos para explicar 95% da variância total? Responda como un único escalar inteiro.
+
+# In[13]:
+
+
+pca_components = PCA(0.95)
+n_components = pca_components.fit_transform(fifa_standardize)
+int(n_components.shape[1])
+
+
+# In[24]:
+
+
+def q2():
+    pca_components = PCA(0.95)
+    n_components = pca_components.fit_transform(fifa)
+    return int(n_components.shape[1])
+
+
+# ## Questão 3
+# 
+# Qual são as coordenadas (primeiro e segundo componentes principais) do ponto `x` abaixo? O vetor abaixo já está centralizado. Cuidado para __não__ centralizar o vetor novamente (por exemplo, invocando `PCA.transform()` nele). Responda como uma tupla de float arredondados para três casas decimais.
+
+# In[15]:
+
+
+x = [0.87747123,  -1.24990363,  -1.3191255, -36.7341814,
+     -35.55091139, -37.29814417, -28.68671182, -30.90902583,
+     -42.37100061, -32.17082438, -28.86315326, -22.71193348,
+     -38.36945867, -20.61407566, -22.72696734, -25.50360703,
+     2.16339005, -27.96657305, -33.46004736,  -5.08943224,
+     -30.21994603,   3.68803348, -36.10997302, -30.86899058,
+     -22.69827634, -37.95847789, -22.40090313, -30.54859849,
+     -26.64827358, -19.28162344, -34.69783578, -34.6614351,
+     48.38377664,  47.60840355,  45.76793876,  44.61110193,
+     49.28911284
+]
+
+
+# In[16]:
+
+
+pca_3 = PCA(n_components = 2).fit(fifa_standardize)
+result = pca_3.components_.dot(x).round(3)
+tuple(result)
+
+
+# In[25]:
+
+
+def q3():
+    pca_3 = PCA(n_components = 2).fit(fifa)
+    result = pca_3.components_.dot(x).round(3)
+    return tuple(result)
+
+
+# ## Questão 4
+# 
+# Realiza RFE com estimador de regressão linear para selecionar cinco variáveis, eliminando uma a uma. Quais são as variáveis selecionadas? Responda como uma lista de nomes de variáveis.
+
+# In[18]:
+
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.feature_selection import RFE
+
+
+# In[19]:
+
+
+fifa.dropna(inplace = True)
+fifa_x = fifa.drop('Overall',1)
+fifa_y = fifa['Overall']
+X_train, X_test, y_train, y_test = train_test_split(fifa_x,  fifa_y, test_size = 0.2)
+sc = StandardScaler()
+X_train = ((X_train - X_train.mean())/X_train.std())
+X_test = ((X_test - X_test.mean())/X_test.std())
+linear = LinearRegression()
+model = linear.fit(X_train, y_train)
+rfe = RFE(linear, 5)
+rfe_fit = rfe.fit(X_train, y_train)
+
+
+# In[20]:
+
+
+resultado = pd.DataFrame({'colunas': X_train.columns, 
+                          'bool':rfe_fit.get_support(),
+                          'coeficientes': pd.Series(linear.coef_)})
+
+
+# In[21]:
+
+
+resultado_coef = resultado[resultado['bool'] == True]
+resultado_final = list(resultado_coef['colunas'])
+resultado_final
+
+
+# In[22]:
+
+
+def q4():
+    fifa.dropna(inplace = True)
+    fifa_x = fifa.drop('Overall',1)
+    fifa_y = fifa['Overall']
+    X_train, X_test, y_train, y_test = train_test_split(fifa_x,  fifa_y, test_size = 0.2)
+    sc = StandardScaler()
+    X_train = ((X_train - X_train.mean())/X_train.std())
+    X_test = ((X_test - X_test.mean())/X_test.std())
+    linear = LinearRegression()
+    model = linear.fit(X_train, y_train)
+    rfe = RFE(linear, 5)
+    rfe_fit = rfe.fit(X_train, y_train)
+    resultado = pd.DataFrame({'colunas': X_train.columns, 
+                              'bool':rfe_fit.get_support(),
+                              'coeficientes': pd.Series(linear.coef_)})
+    resultado_coef = resultado[resultado['bool'] == True]
+    resultado_final = list(resultado_coef['colunas'])
+    return resultado_final
+    
+
